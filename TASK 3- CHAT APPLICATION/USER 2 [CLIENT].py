2c9de012fc234bbb2c2a334db875f593a95f86ef
chaman2003 <156411533+chaman2003@users.noreply.github.com>
2024-09-25 11:01:05 +0530

    .

--- TASK 3- CHAT APPLICATION/USER 2 [CLIENT].py
+++ TASK 3- CHAT APPLICATION/USER 2 [CLIENT].py
@@ -10,21 +10,17 @@ def receive_messages(client):
             print("[ERROR]", e)
             break
 
-# Client setup
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 5555))
 
-# Start a thread to receive messages
 receive_thread = threading.Thread(target=receive_messages, args=(client,))
 receive_thread.start()
 
-# Main loop to send messages
 first_message_received = False
 while True:
     message = input()
     client.send(message.encode())
     
-    # Check if this is the first message from the server
     if not first_message_received:
         print(f"[User 1] Welcome to the chat server!")
         first_message_received = True
